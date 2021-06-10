eb2541db2434d9ada4c5064b364fdab42309fb68
Msrinath-71 <84717639+Msrinath-71@users.noreply.github.com>
2021-06-10 16:17:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-//this is test//
+//this is test change
 ReactDOM.render(<App />, document.getElementById('root'));
 
 
