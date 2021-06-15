b73afb446be332845eddfe957be914d6d094fdee
Msrinath-71 <84717639+Msrinath-71@users.noreply.github.com>
2021-06-15 19:25:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 //this is demo 
+//this is test conflict
 ReactDOM.render(<App />, document.getElementById('root'));
 
 
