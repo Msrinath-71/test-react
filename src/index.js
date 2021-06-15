1d70eaae1ba20faab558b8c05caefcbd4ddcbf3d
Msrinath-71 <84717639+Msrinath-71@users.noreply.github.com>
2021-06-15 18:53:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-//this is demo repo
+//this is demo repo created
 ReactDOM.render(<App />, document.getElementById('root'));
 
 
